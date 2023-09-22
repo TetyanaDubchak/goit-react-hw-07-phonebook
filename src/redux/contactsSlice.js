a2cf1f23bccf49f1351dcb16a15ac4649eff24cf
TetyanaDubchak <123674629+TetyanaDubchak@users.noreply.github.com>
2023-09-22 19:55:21 +0300

    remove comments

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -49,6 +49,5 @@ const contactsSlice = createSlice({
 
 })
 
-// export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,45 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist'
-// import storage from 'redux-persist/lib/storage'
-
-// const rootReducer = combineReducers({
-//     contacts: contactsReducer,
-//     filters: filtersReducer,
-// })
-
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-//   blacklist: ['filters'],
-// }
-
-// const persistedReducer = persistReducer(persistConfig,rootReducer)
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filters: filtersReducer
   }
-  // reducer: persistedReducer,
-  // middleware(getDefaultMiddleware) {
-  //     return getDefaultMiddleware({
-  //       serializableCheck: {
-  //         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  //       },
-  //     });
-  //   },
+
 });
 
-// export const persistor = persistStore(store)
+
 
